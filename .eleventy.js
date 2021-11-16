af1d02c3cbc9c93d546d118ae44894a625f7dd58
jsahlsa <joe.sahlstrom@gmail.com>
2021-11-16 22:21:10 +0000

    droped files in gitignore

--- .eleventy.js
+++ .eleventy.js
@@ -1,1 +1,5 @@
-module.exports = (function (eleventyConfig) { eleventyConfig.addFilter("filesize", function (path) { return "0 KB"; });});
+module.exports = (function (eleventyConfig) {
+    eleventyConfig.addFilter("filesize", function (path) {
+        return "0 KB";
+    });
+});
