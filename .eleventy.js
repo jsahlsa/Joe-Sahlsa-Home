621490065af0497d7c547c42434be5a36d773b89
jsahlsa <joe.sahlstrom@gmail.com>
2021-11-23 23:59:15 +0000

    ☕ playing with variable fonts

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/style.css');
   eleventyConfig.addPassthroughCopy('src/index.js');
+  eleventyConfig.addPassthroughCopy('src/fonts');
 
   return {
     passthroughFileCopy: true,
