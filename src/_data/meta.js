048dec19447ffc0da58875102468a896b396e92e
jsahlsa <joe.sahlstrom@gmail.com>
2021-11-30 00:06:28 +0000

    implemented about and blog pages, still confused

---
+++ src/_data/meta.js
@@ -0,0 +1,7 @@
+module.exports = {
+  url: process.env.URL || 'http://localhost:8080',
+  siteName: 'Joe Sahlsa',
+  siteDescription: 'Website for an aspiring web developer',
+  authorName: 'Joe Sahlsa',
+  twitterUsername: 'joesahlsa', // no `@`
+};
