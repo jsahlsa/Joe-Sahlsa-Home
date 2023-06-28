63f3067675832754b96cf1c6283e2d218d14cf96
jsahlsa <joe.sahlstrom@gmail.com>
2023-06-28 15:33:37 -0700

    added webmentions to site

--- utils/filters.js
+++ utils/filters.js
@@ -2,21 +2,23 @@ const sanitizeHTML = require('sanitize-html');
 
 module.exports = {
   getWebmentionsForUrl: function (webmentions, url) {
-    const allowedTypes = [
-      'mention-of',
-      'in-reply-of',
-      'like-of',
-      'repost-of',
-      'in-reply-to',
-    ];
+    // const allowedTypes = [
+    //   'mention-of',
+    //   'in-reply-of',
+    //   'like-of',
+    //   'repost-of',
+    //   'in-reply-to',
+    // ];
 
     const orderByDate = (a, b) => {
       return new Date(a.published) - new Date(b.published);
     };
-    return webmentions
-      .filter((entry) => entry['wm-target'] === url)
-      .filter((entry) => allowedTypes.includes(entry['wm-property']))
-      .sort(orderByDate);
+    return (
+      webmentions
+        // .filter((entry) => entry['wm-target'] === url)
+        // .filter((entry) => allowedTypes.includes(entry['wm-property']))
+        .sort(orderByDate)
+    );
   },
   sanitizeHTML: function (entry) {
     const { html } = entry.content;
